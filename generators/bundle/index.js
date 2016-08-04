fa6289016420735d117b25161328988c85b885e1
Julian <julkuh@googlemail.com>
2016-08-04 14:50:23 +0200

    -- allow creation of component

--- generators/bundle/index.js
+++ generators/bundle/index.js
@@ -25,30 +25,37 @@ module.exports = yeoman.Base.extend({
             {
                 type: 'input',
                 name: 'name',
-                message: 'How shall your bundle be named?',
+                message: 'How shall the bundle be named?',
                 default: 'mybundle'
-            }, {
+            },
+            /*{
                 type: 'input',
                 name: 'version',
                 message: 'What\'s the initital Verison of your bundle?',
                 default: '0.0.1'
-            }, {
+            } */
+            {
                 type: 'input',
                 name: 'description',
-                message: 'Describe briefly what great stuff your bundle is going to do',
+                message: 'Describe briefly what great stuff this bundle is going to do',
                 default: 'Add description later'
             }, {
                 type: 'confirm',
                 name: 'skipI18n',
                 message: 'Skip creation of i18n?',
                 default: false
+            }, {
+                type: 'input',
+                name: 'componentName',
+                message: 'Give a name for a first Bundle Component.',
+                default: 'MyFirstComponent'
             }
             // TODO:
             /*
             ,{
                 type: 'confirm',
                 name: 'widgetCreation',
-                message: 'Do you want to create a Widget, Window and ToggleTool?',
+                message: 'Does your bundle need a UI? If YES, let's create a Widget, Window and ToggleTool. If NO, an empty Component and its JS File is created.',
                 default: no
             },{
                 type: 'confirm',
@@ -63,18 +70,31 @@ module.exports = yeoman.Base.extend({
         }.bind(this));
     },
     writing: function () {
+        this.manifest = {}
+        this.manifest.components = '';
+        this.module = {
+            define: ''
+        };
+
+        this.component = {
+            name: this.answers.componentName
+        };
+
         //convert whitespaces in bundle name to "-" to use it as bundle-id and folder name.
         this.bundeleID = this.answers.name.replace(/\s/g, "-");
         this.bundleFolder = this.bundeleID;
 
+        this._createComponentInManifest(this.component.name);
+
         // with i18n
         if (!this.answers.skipI18n) {
-            this.bundleLocalization = '\n';
+            this.manifest.bundleLocalization = '\n';
+            this.manifest.bundlename = '${bundleName}';
+            this.manifest.description = '${bundleDescription}';
+
+            this.module.define = '"."';
 
             this._copyFile('main.js', {});
-            this._copyFile('module.js', {
-                moduleDefine: '"."'
-            });
             this._copyFile('main.js', {
                 defineString: "dojo/i18n!./nls/bundle"
             });
@@ -86,32 +106,38 @@ module.exports = yeoman.Base.extend({
                 name: this.answers.name,
                 description: this.answers.description
             });
-
-            this._copyFile('manifest.json', {
-                id: this.bundeleID,
-                name: '${bundleName}',
-                version: this.answers.version,
-                description: '${bundleDescription}',
-                bundleLocalization: this.bundleLocalization
-            });
-
         }
 
         // without i18n
         else {
-            this.bundleLocalization = '"Bundle-Localization": [],\n\t"Bundle-Main": "",';
-            this._copyFile('module.js', {
-                moduleDefine: ''
-            });
+            this.manifest.bundleLocalization = '"Bundle-Localization": [],\n\t"Bundle-Main": "",';
+            this.manifest.bundlename = this.answers.name;
+            this.manifest.description = this.answers.description;
 
-            this._copyFile('manifest.json', {
-                id: this.bundeleID,
-                name: this.answers.name,
-                version: this.answers.version,
-                description: this.answers.description,
-                bundleLocalization: this.bundleLocalization
-            });
+            this.module.define = '"."';
         }
+
+        // manifest.json
+        this._copyFile('manifest.json', {
+            id: this.bundeleID,
+            name: this.manifest.bundlename,
+            version: '0.0.1',
+            description: this.manifest.description,
+            bundleLocalization: this.manifest.bundleLocalization,
+            components: this.manifest.components
+        });
+
+        // module.js
+        this.module.define += ',\n\t"' + this.component.name + '"';
+        this._copyFile('module.js', {
+            moduleDefine: this.module.define
+        });
+
+        //componentname.js
+        this.fs.copyTpl(
+            this.templatePath('ComponentName.js'),
+            this.destinationPath(this.bundleFolder + '/' + this.component.name + ".js"), {}
+        );
     },
 
     _copyFile(filename, replacements) {
@@ -119,6 +145,13 @@ module.exports = yeoman.Base.extend({
             this.templatePath(filename),
             this.destinationPath(this.bundleFolder + '/' + filename), replacements
         );
+    },
+
+    _createComponentInManifest(name) {
+        var componentToAdd = {
+            "name": name
+        };
+        this.manifest.components = '[{\n\t\t"name": "' + name + '"\n\t}]';
     }
 
     //    method1: function () {
@@ -127,5 +160,6 @@ module.exports = yeoman.Base.extend({
     //    method2: function () {
     //        console.log('method 2 just ran');
     //    }
+
 
 });
---
+++ generators/bundle/templates/ComponentName.js
@@ -0,0 +1,7 @@
+define(["dojo/_base/declare"],
+    function  (declare) {
+        return  declare([], {
+            // add some fancy logic here
+        });
+    }
+);
