6c60f7dbda867aa5cd0ad2b5dcbe50342c63a3be
Julian <julkuh@googlemail.com>
2016-08-08 10:09:36 +0200

    -- add a basic test

--- test/app.js
+++ test/app.js
@@ -4,15 +4,17 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-mapapps:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({name: 'mytestapp'})
-      .toPromise();
-  });
+    before(function () {
+        return helpers.run(path.join(__dirname, '../generators/app'))
+            .withPrompts({
+                name: 'mytestapp'
+            })
+            .toPromise();
+    });
 
-  it('creates files', function () {
-    assert.file([
+    it('creates files', function () {
+        assert.file([
       'mytestapp/app.json'
     ]);
-  });
+    });
 });
