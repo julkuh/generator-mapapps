8a8d1b9468e2b4941f7f54c867396f472a4093ed
julkuh <julkuh@googlemail.com>
2016-08-09 19:45:19 +0200

    update tests

--- test/bundle.js
+++ test/bundle.js
@@ -4,17 +4,46 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-mapapps:bundle', function () {
-    before(function () {
-        return helpers.run(path.join(__dirname, '../generators/bundle'))
-            .withPrompts({
-                name: 'mytestbundle'
-            })
-            .toPromise();
-    });
+before(function () {
+    return helpers.run(path.join(__dirname, '../generators/bundle'))
+        .withPrompts({
+            name: 'myTestBundle',
+            skipI18n: true
+        })
+        .toPromise();
+});
 
-    it('creates files', function () {
-        assert.file([
-      'mytestbundle/manifest.json'
+it('creates files', function () {
+    assert.file([
+            'myTestBundle/manifest.json',
+            'myTestBundle/MyFirstComponent.js'
     ]);
-    });
+});
+});
+
+//TODO: Implement TestSuite
+
+//describe('generator-mapapps:bundle', function () {
+//    before(function () {
+//        return helpers.run(path.join(__dirname, '../generators/bundle'))
+//            .withPrompts({
+//                name: 'myTestBundle',
+//                skipI18n: false,
+//                componentName: 'myTestComponent'
+//            })
+//            .toPromise();
+//    });
+//
+//    it('creates files', function () {
+//        assert.file([
+//            'myTestBundle/manifest.json',
+//            'myTestBundle/module.json',
+//            'myTestBundle/main.json',
+//            'myTestBundle/myTestComponent.js',
+//            'myTestBundle/nls/bundle.js'
+//            'myTestBundle/nls/de/bundle.js',
+//
+//    ]);
+//    });
+
 });
