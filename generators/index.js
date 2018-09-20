09024708831e73966c4b6eebcc33e026ad516003
Julian Kuhlmann <j.kuhlmann@conterra.de>
2018-09-20 13:10:45 +0200

    identify if generator is executed inside an already existing bundle

--- generators/index.js
+++ generators/index.js
@@ -5,40 +5,52 @@ var yosay = require('yosay');
 //var beautify = require('gulp-beautify');
 
 module.exports = class extends Generator {
-
-    initializing() { }
+    
+    initializing() {
+        this.isAlreadyBundle = this.fs.exists(this.destinationPath('manifest.json'));
+        this.answers= undefined;
+    }
 
     prompting() {
-        this.log(yosay(
-            'Welcome to ' + chalk.yellow('generator-mapapps') + ' generator! Let\'s create some awesome ' + chalk.green('map.apps stuff')
-        ));
+        if(this.isAlreadyBundle){
+            this.log(yosay(chalk.white.bgRed.bold('Heads up!') +  'You\'ve already created a bundle here. So i assume you wnat to create a new component instead.'));
+        }
+        else {
+            this.log(yosay(
+                'Welcome to ' + chalk.yellow('generator-mapapps') + ' generator! Let\'s create some awesome ' + chalk.green('map.apps stuff')
+            ));
 
-        var prompts = [{
-            type: 'list',
-            name: 'subgenerator',
-            message: 'Building a bundle or an app ?',
-            choices: ['bundle4x', 'bundle3x', 'app3x'],
-            default: 'bundle4x'
-        }];
+            var prompts = [{
+                type: 'list',
+                name: 'subgenerator',
+                message: 'Building a bundle or an app ?',
+                choices: ['bundle4x','bundle3x','app3x'],
+                default: 'bundle4x'
+            }];
 
-        return this.prompt(prompts).then(function (answers) {
-            this.answers = answers;
-        }.bind(this));
+            return this.prompt(prompts).then(function (answers) {
+                this.answers = answers;
+            }.bind(this));
+        }
     }
 
 
     configuring() {
-        if (this.answers.subgenerator === 'bundle4x') {
-            this.composeWith('mapapps:bundle4x');
+        if (this.isAlreadyBundle){
+            this.composeWith('mapapps:bundle-add-component');
         }
-        if (this.answers.subgenerator === 'bundle3x') {
-            this.composeWith('mapapps:bundle3x');
-        }
-        if (this.answers.subgenerator === 'app3x') {
-            this.composeWith('mapapps:app3x');
+        else {
+            if (this.answers.subgenerator === 'bundle4x') {
+                this.composeWith('mapapps:bundle4x');
+            }
+            if (this.answers.subgenerator === 'bundle3x') {
+                this.composeWith('mapapps:bundle3x');
+            }
+            if (this.answers.subgenerator === 'app3x') {
+                this.composeWith('mapapps:app3x');
+            }
         }
     }
-
 
     install() {
         // TODO: implement beautify of files with gulp here
