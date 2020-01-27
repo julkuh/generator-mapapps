00f059c99459a93099b8f2d6ca5c859ad1273e53
Julian Kuhlmann <j.kuhlmann@conterra.de>
2020-01-27 15:12:09 +0100

    prepare vue file linting

--- generators/bundle4x/templates/ComponentName.vue
+++ generators/bundle4x/templates/ComponentName.vue
@@ -1,29 +1,47 @@
 <template>
-    <v-container fluid class="pa-0">
+    <v-container
+        fluid
+        class="pa-0"
+    >
         <v-layout row>
-            <span>{{ i18n.ui.intro }}</span>
+            <span>
+                {{ i18n.ui.intro }}
+            </span>
         </v-layout>
         <v-layout row>
-            <v-text-field v-model="name" class="my-2" name="Name" :label=i18n.ui.textinput hide-details></v-text-field>
+            <v-text-field
+                v-model="name"
+                class="my-2"
+                name="Name"
+                :label="i18n.ui.textinput"
+                hide-details
+            />
         </v-layout>
         <v-layout row>
-            <v-btn color="primary" block @click="clickMe">{{ i18n.ui.windowTitle }} Click me!</v-btn>
+            <v-btn
+                color="primary"
+                block
+                @click="clickMe"
+            >
+                {{ i18n.ui.windowTitle }} Click me!
+            </v-btn>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      name: "",
-      i18n: Object
-    };
-  },
-  methods: {
-    clickMe() {
-      confirm("your Name is " + this.name + " ?");
-    }
-  }
-};
+  export default {
+      data: function() {
+          return {
+              name: "",
+              i18n: Object
+          };
+      },
+      methods: {
+          clickMe() {
+          // eslint-disable-next-line no-alert
+              confirm(`Your name is ${this.name} ?`);
+          }
+      }
+  };
 </script>
