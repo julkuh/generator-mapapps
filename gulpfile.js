21c17cb18e6130481e0c3368b3875fbea1c34f23
Julian Kuhlmann <j.kuhlmann@conterra.de>
2020-01-28 07:57:37 +0100

    remove nsp as it is no longer supported

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
 
@@ -15,12 +14,6 @@ gulp.task('static', function () {
     //.pipe(eslint())
     //.pipe(eslint.format())
     //.pipe(eslint.failAfterError());
-});
-
-gulp.task('nsp', function (cb) {
-    nsp({
-        package: path.resolve('package.json')
-    }, cb);
 });
 
 gulp.task('pre-test', function () {
@@ -62,5 +55,5 @@ gulp.task('coveralls', ['test'], function () {
         .pipe(coveralls());
 });
 
-gulp.task('prepublish', ['nsp']);
+gulp.task('prepublish', ['default']);
 gulp.task('default', ['static', 'test', 'coveralls']);
