3edff54e25f42a8bd65c251d174e5259aa7f98d2
Julian Kuhlmann <j.kuhlmann@conterra.de>
2020-01-29 10:44:46 +0100

    adjust sample vue template to contain a simple flex based dialog layout with scrollable center area

--- generators/bundle4x/templates/ComponentName.vue
+++ generators/bundle4x/templates/ComponentName.vue
@@ -1,47 +1,73 @@
 <template>
     <v-container
-        fluid
-        class="pa-0"
+            fluid
+            fill-height
+            class="my-widget pa-0"
     >
-        <v-layout row>
-            <span>
-                {{ i18n.ui.intro }}
-            </span>
-        </v-layout>
-        <v-layout row>
-            <v-text-field
-                v-model="name"
-                class="my-2"
-                name="Name"
-                :label="i18n.ui.textinput"
-                hide-details
-            />
-        </v-layout>
-        <v-layout row>
-            <v-btn
-                color="primary"
-                block
-                @click="clickMe"
+        <v-layout column>
+            <!-- optional Header Content Area  (constant height, can be used multiple times) -->
+            <v-flex
+                    shrink
+                    class="my-widget__header"
             >
-                {{ i18n.ui.windowTitle }} Click me!
-            </v-btn>
+                <v-alert
+                        :value="true"
+                        type="info"
+                >
+                    This is a simple demo to show how simple dialog design works
+                </v-alert>
+            </v-flex>
+            <!-- Main Content Area (Grows, shrinks and scrolls if needed) -->
+            <v-flex
+                    style="overflow: auto"
+                    class="my-widget__center"
+            >
+                <v-text-field
+                        v-model="name"
+                        class="my-2"
+                        name="Name"
+                        :label="i18n.ui.textinput"
+                        hide-details
+                />
+                <v-textarea
+                        label="Leave a message"
+                        :value="message"
+                        hide-details
+                />
+            </v-flex>
+            <v-divider class="mb-1" />
+            <!-- optional Bottom Bar (constant height) -->
+            <v-flex
+                    shrink
+                    class="my-widget__bottom-bar"
+            >
+                <v-btn
+                        color="primary"
+                        block
+                        mx-0
+                        @click="clickMe"
+                >
+                    {{ i18n.ui.windowTitle }} Click me!
+                </v-btn>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-  export default {
-      data: function() {
-          return {
-              name: "",
-              i18n: Object
-          };
-      },
-      methods: {
-          clickMe() {
-          // eslint-disable-next-line no-alert
-              confirm(`Your name is ${this.name} ?`);
-          }
-      }
-  };
+    export default {
+        data: function() {
+            return {
+                name: "",
+                i18n: Object,
+                message: ""
+            };
+        },
+        methods: {
+            clickMe() {
+                // eslint-disable-next-line no-alert
+                confirm(`Your name is ${this.name}. And the message was ${this.message}` );
+            }
+        }
+    };
 </script>
